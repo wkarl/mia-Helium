66cfffe95c5ee203d9dafef32a43995cfdf3a3b3
Wolfgang Karl <kontakt@wkarl.net>
2015-11-03 16:34:07 +0100

    Add missing hashCode() method (generated by studio)

--- helium/src/main/java/de/prosiebensat1digital/helium/model/DeviceInfo.java
+++ helium/src/main/java/de/prosiebensat1digital/helium/model/DeviceInfo.java
@@ -22,6 +22,13 @@ public class DeviceInfo {
     }
 
     @Override
+    public int hashCode() {
+        int result = mApp != null ? mApp.hashCode() : 0;
+        result = 31 * result + (mDevice != null ? mDevice.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public boolean equals(final Object other) {
         return other instanceof DeviceInfo && mApp.equals(((DeviceInfo) other).mApp) && mDevice.equals(((DeviceInfo) other).mDevice);
     }
@@ -34,6 +41,13 @@ public class DeviceInfo {
         public App(@JsonProperty("id") String inName, @JsonProperty("version") String inVersion) {
             mName = inName;
             mVersion = inVersion;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = mName != null ? mName.hashCode() : 0;
+            result = 31 * result + (mVersion != null ? mVersion.hashCode() : 0);
+            return result;
         }
 
         @Override
@@ -55,6 +69,13 @@ public class DeviceInfo {
         }
 
         @Override
+        public int hashCode() {
+            int result = mModel != null ? mModel.hashCode() : 0;
+            result = 31 * result + (mPlatform != null ? mPlatform.hashCode() : 0);
+            return result;
+        }
+
+        @Override
         public boolean equals(final Object other) {
             return other instanceof Device
                     && CompareUtils.equals(mModel, ((Device) other).mModel)
@@ -70,6 +91,13 @@ public class DeviceInfo {
         public Platform(@JsonProperty("type") String inType, @JsonProperty("version") String inVersion) {
             mType = inType;
             mVersion = inVersion;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = mType != null ? mType.hashCode() : 0;
+            result = 31 * result + (mVersion != null ? mVersion.hashCode() : 0);
+            return result;
         }
 
         @Override
