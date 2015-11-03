ecdc88839dbc88d3885dd69d3ef9e383a8f2040f
Wolfgang Karl <kontakt@wkarl.net>
2015-11-03 14:28:49 +0100

    Fix usage of equals and Json property annotations

--- helium/src/main/java/de/prosiebensat1digital/helium/model/DeviceInfo.java
+++ helium/src/main/java/de/prosiebensat1digital/helium/model/DeviceInfo.java
@@ -7,88 +7,76 @@ import de.prosiebensat1digital.helium.util.CompareUtils;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DeviceInfo {
-    @JsonProperty("app")
-    public App    mApp;
-    @JsonProperty("device")
+    public App mApp;
     public Device mDevice;
-    
-    public DeviceInfo() {
+
+    public DeviceInfo(@JsonProperty("app") App inApp, @JsonProperty("device") Device inDevice) {
+        mApp = inApp;
+        mDevice = inDevice;
     }
-    
+
     public DeviceInfo(String inAppName, String inAppVersion, String inModel, String inPlatformName,
                       String inPlatformVersion) {
         mApp = new App(inAppName, inAppVersion);
         mDevice = new Device(inModel, new Platform(inPlatformName, inPlatformVersion));
     }
-    
-    public boolean equals(final DeviceInfo other) {
-        return other != null && mApp != null && mDevice != null
-               && mApp.equals(other.mApp) && mDevice.equals(other.mDevice);
+
+    @Override
+    public boolean equals(final Object other) {
+        return other instanceof DeviceInfo && mApp.equals(((DeviceInfo) other).mApp) && mDevice.equals(((DeviceInfo) other).mDevice);
     }
-    
+
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class App {
-        @JsonProperty("id")
         public String mName;
-        @JsonProperty("version")
         public String mVersion;
-        
-        public App() {
-        }
-        
-        public App(final String inName, final String inVersion) {
+
+        public App(@JsonProperty("id") String inName, @JsonProperty("version") String inVersion) {
             mName = inName;
             mVersion = inVersion;
         }
-        
-        public boolean equals(final App other) {
-            return other != null
-                   && CompareUtils.equals(mName, other.mName)
-                   && CompareUtils.equals(mVersion, other.mVersion);
+
+        @Override
+        public boolean equals(final Object other) {
+            return other instanceof App
+                    && CompareUtils.equals(mName, ((App) other).mName)
+                    && CompareUtils.equals(mVersion, ((App) other).mVersion);
         }
     }
-    
+
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Device {
-        @JsonProperty("model")
-        public String   mModel;
-        @JsonProperty("os")
+        public String mModel;
         public Platform mPlatform;
-        
-        public Device() {
-        }
-        
-        public Device(final String inModel, final Platform inPlatform) {
+
+        public Device(@JsonProperty("model") String inModel, @JsonProperty("os") Platform inPlatform) {
             mModel = inModel;
             mPlatform = inPlatform;
         }
-        
-        public boolean equals(final Device other) {
-            return other != null
-                   && CompareUtils.equals(mModel, other.mModel)
-                   && mPlatform.equals(other.mPlatform);
+
+        @Override
+        public boolean equals(final Object other) {
+            return other instanceof Device
+                    && CompareUtils.equals(mModel, ((Device) other).mModel)
+                    && mPlatform.equals(((Device) other).mPlatform);
         }
     }
-    
+
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Platform {
-        @JsonProperty("type")
         public String mType;
-        @JsonProperty("version")
         public String mVersion;
-        
-        public Platform() {
-        }
-        
-        public Platform(final String inType, final String inVersion) {
+
+        public Platform(@JsonProperty("type") String inType, @JsonProperty("version") String inVersion) {
             mType = inType;
             mVersion = inVersion;
         }
-        
-        public boolean equals(final Platform other) {
-            return other != null
-                   && CompareUtils.equals(mType, other.mType)
-                   && CompareUtils.equals(mVersion, other.mVersion);
+
+        @Override
+        public boolean equals(final Object other) {
+            return other instanceof Platform
+                    && CompareUtils.equals(mType, ((Platform) other).mType)
+                    && CompareUtils.equals(mVersion, ((Platform) other).mVersion);
         }
     }
 }
